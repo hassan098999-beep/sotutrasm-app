bc542f75c4b7c5a62697dcc5cfb46e97b34661bc
hassan098999-beep <hassan098999@gmail.com>
2026-01-20 01:34:51 +0100

    Update UI - French modern PWA

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,26 @@
-const CACHE_NAME = "pwa-v1";
-const FILES = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json'
+const CACHE_NAME = 'pwa-service-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icon1.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
 });
